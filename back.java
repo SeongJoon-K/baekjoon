5b7cdee4cb7574c3a68a56add6c73d75e5dbaa23
hisocean <jobcho6320@naver.com>
2022-01-20 23:17:26 +0900

    120_for

---
+++ back.java
@@ -0,0 +1,13 @@
+public class back {
+    public static void main (String[] args) {
+        
+        int N = 30;
+        for(int i = N; i > 0 ; i--) {
+            for(int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
