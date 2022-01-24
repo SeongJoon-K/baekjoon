e3b9b3742104399ededc3703d924c925e54b45b3
hisocean <jobcho6320@naver.com>
2022-01-24 11:26:40 +0900

    124_dfspratice

---
+++ checkarr.java
@@ -0,0 +1,31 @@
+public class checkarr {
+    public static void main(String[] args) {
+        int arr[4];
+        int check[7];
+        int n=6;
+        for(int i=0;i<=n;i++){
+            if(check[i]==0){
+            check[i]=1;
+            arr[1]=i;
+            for(int j=0;j<=n;j++){
+                if(check[j]==0){
+                check[j]=1;
+                arr[2]=j;
+                    for(int k=0;k<=n;k++){ 
+                        if(check[k]==0){
+                        check[k]=1;
+                        arr[3]=k;
+                        System.out.println("%d , %d , %d",arr[1],arr[2],arr[3]);
+                        printf("%d , %d , %d, %d , %d, %d",check[1],check[2],check[3],check[4],check[5],check[6]);
+                        check[k]=0;
+                            }
+                        }
+                    check[j]=0;
+                    }  
+                }
+            check[i]=0;
+            }
+        }
+    }
+}
+
