18ec007cd4dc5aa37f4f871fc13e495b91210c69
hisocean <jobcho6320@naver.com>
2022-01-25 23:53:46 +0900

    125_arraypratice

--- baek_1.java
+++ baek_1.java
@@ -3,13 +3,23 @@ import java.util.Scanner;
 public class baek_1 {
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
-        System.out.print("정수 a입력 : ");
-        int A = sc.nextInt();
-        System.out.print("정수 b입력 : ");
-        int B = sc.nextInt();
-        System.out.println(A-B);
-        sc.close();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("정수 a입력 : ");
+        // int A = sc.nextInt();
+        // System.out.print("정수 b입력 : ");
+        // int B = sc.nextInt();
+        // System.out.println(A-B);
+        // sc.close();
+
+        int [][] arr = new int [5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j ++) {
+                arr[j] = j;
+            }
+            arr[i] = i;
+        }
+
+
     }
     
 }
