4e8bc034839aa66b53535a61fc79c0efbf3dffdc
hisocean <jobcho6320@naver.com>
2022-01-27 01:31:59 +0900

    127_arraypra

--- checkarr.java
+++ checkarr.java
@@ -1,31 +1,26 @@
-// public class checkarr {
-//     public static void main(String[] args) {
-//         int arr[4];
-//         int check[7];
-//         int n=6;
-//         for(int i=0;i<=n;i++){
-//             if(check[i]==0){
-//             check[i]=1;
-//             arr[1]=i;
-//             for(int j=0;j<=n;j++){
-//                 if(check[j]==0){
-//                 check[j]=1;
-//                 arr[2]=j;
-//                     for(int k=0;k<=n;k++){ 
-//                         if(check[k]==0){
-//                         check[k]=1;
-//                         arr[3]=k;
-//                         System.out.println("%d , %d , %d",arr[1],arr[2],arr[3]);
-//                         printf("%d , %d , %d, %d , %d, %d",check[1],check[2],check[3],check[4],check[5],check[6]);
-//                         check[k]=0;
-//                             }
-//                         }
-//                     check[j]=0;
-//                     }  
-//                 }
-//             check[i]=0;
-//             }
-//         }
-//     }
-// }
+public class checkarr {
+    public static void main(String[] args) {
+
+
+        // int arrayInt[] = new int[10];
+        // for (int i = 0; i < arrayInt.length; i++) {
+        //     System.out.println(i);
+        // }
+
+        // int arrayInt2[] = {1,3,5,7,9};
+        // // for (int i = 0; i < arrayInt.length; i++) {
+        // //     System.out.println(i);
+        // // }
+        // // 안에 있는 값을 포문으로 하나씩 출력하기
+        // for (int i = 0; i <= arrayInt2.length; i++) {
+        //     System.out.println(arrayInt2[i]);
+        // }
+        
+        String arrayStr[] = {"a","b","c","d","e"};
+        for (int i = 0; i < arrayStr.length; i++) {
+            System.out.println(arrayStr[i]);
+        }
+    
+    }
+}
 
