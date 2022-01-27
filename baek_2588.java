482339e21cdfe5a344b282703857bf4ab2bb9565
hisocean <jobcho6320@naver.com>
2022-01-27 11:03:56 +0900

    127_2753

--- baek_2588.java
+++ baek_2588.java
@@ -1,21 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class baek_2588 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        // int[] a1 = new int[a];
-        int[] b1 = new int[b]; // 여기서 a1,b1은array의 주솟값
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(b[1]);
-        for (int i = 0; i < 3; i++) {
-            System.out.println(a*b1[i]);
-    
-    
-        }
+        System.out.println(a*(b % 10));
+        System.out.println(a*(b % 100 / 10));
+        System.out.println(a*(b/100));
+        System.out.println(a*b);
     }
 }
     
