af5f3da095ff5b1a493aeaf7054b34176a99816a
SeongJoon-K <jobcho6320@naver.com>
2022-02-02 16:41:03 +0900

    202_1546

---
+++ baek_1546.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class baek_1546 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        double[] score = new double[N];
+        double max = 0;
+        double sum = 0;
+        for (int i = 0; i < score.length; i++) {
+            score[i] = sc.nextDouble();
+            if (score[i] > max) {
+                max = score[i];
+            }
+        }
+        System.out.println("max값" + max);
+        for (int i = 0; i < score.length; i++) {
+            
+            score[i] = score[i]*100/max;
+
+            System.out.println("이거 바꾼점수 값"+score[i]);
+            sum = score[i] + sum;
+        }
+        System.out.println(sum/N);
+    }
+}
