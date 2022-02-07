83a0e9c47c5ff329bbca4057a7f99ec3e41771aa
SeongJoon-K <jobcho6320@naver.com>
2022-02-08 00:22:59 +0900

    208_11654

---
+++ baek_11654.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class baek_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a;
+        a = sc.next().charAt(0); // 문자열 리턴
+        System.out.println((int)a);
+
+        // charAt(x)의 경우 저장된 문자열 중 한 글자만 선택해서 char로 변환
+        // 여기서 x는 인덱스의 번호이며 
+    }
+}
