c31f5afdd8501afa4d7b170cb68c18fa79f0ed2a
SeongJoon-K <jobcho6320@naver.com>
2022-02-09 14:49:36 +0900

    209_1157

--- baek_1157.java
+++ baek_1157.java
@@ -4,34 +4,28 @@ import java.util.Scanner;
 public class baek_1157 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str1 = sc.next();
-        String str = str1.toLowerCase();
-        System.out.println(str);
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char i = 'a'; i <= 'z'; i++) {
-            map.put(i,0);
-        }
-        for(int i = 0; i < str.length(); i++){
-            char cha = str.charAt(i);
-            map.put(cha,map.get(cha)+1);
-        }
-        char max = 'a';
-        for(char key : map.keySet()){
-            if(map.get(key) > map.get(max)) max = key;
-        }
-        char answer ='1';
-        for(char key : map.keySet()){
-            if(max == key) continue;
-            else{
-                if(map.get(max) == map.get(key)) answer ='?';
+
+        int[] arr = new int[26];
+        String str = sc.next();
+        int max = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (65 <= str.charAt(i) && str.charAt(i) <= 90){
+                arr[str.charAt(i)-65]++;
+            } else {
+                arr[str.charAt(i)-97]++;
             }
         }
-        if(answer == '?'){
-            System.out.println(answer);    
+        char ch = 'a';
+        for (int i = 0; i < 26; i++){
+            if(arr[i] > max) {
+                max = arr[i];
+                ch = (char)(i+65);
+            } else if (arr[i]==max) {
+                ch = '?';
+            }
         }
-        else{
-            answer = (char)(max - 32);
-            System.out.println(answer);    
-        }
+        System.out.println(ch);
+        
+
     }
 }
--- que.java
+++ que.java
@@ -16,3 +16,21 @@ public class que {
     }
     
 }
+/*
+큐 (Queue)
+-FIFO형 자료구조
+-자바에서 Queue는 인터페이스이므로 객체 생성이 불가능함.
+-그러므로 LinkedList 형변환을 하는 거임.
+
+Queue = ["데이터1", "데이터2","데이터3"]  
+여기서 front = 0(젤 앞에 있는거) rear = 2 (젤 뒤에 있는거)
+
+queue.peek() 는 큐의 front값을 반환
+queue.poll() 는 큐의 front값을 반환 후 삭제한다.
+스택의 경우 push pop이지만, 큐는 poll임
+queue.offer(값) 은 큐에 값을 추가한다.
+queue.isEmpty() false
+
+
+*/
+
