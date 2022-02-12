01f065b0d5a7369469da72d83a63b38db7b722c5
SeongJoon-K <jobcho6320@naver.com>
2022-02-12 23:11:45 +0900

    212_1712

---
+++ baek_1712.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class baek_1712 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(); // 고정비용
+        int B = sc.nextInt(); // 개당 생산단가
+        int C = sc.nextInt(); // 판매가격
+        int price = C - B;
+        int i;            // 생산 물건 갯수
+
+        if (B >= C) {         // 생산단가가 판매가보다 비쌀 때
+            i = -1;
+            System.out.println();
+        } else {
+            i = A/price + 1;
+            System.out.println(i);
+        }
+    }
+}
