43779967958e888a27f30f5162ae5ccb6bd2b9f1
SeongJoon-K <jobcho6320@naver.com>
2022-02-17 23:40:20 +0900

    217_2775 보완 필요

---
+++ baek_2775.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class baek_2775 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // 테스트 횟수
+        // 내가 2층 3호에 살려면, 101호, 102호, 103호의 사람수의 합만큼 데리고 와야함
+        
+        for (int i = 0; i < T; i++) {
+            int k = sc.nextInt(); // 층 수
+            int n = sc.nextInt(); // 호 수
+            int[] height = new int[k];
+            int[] hosu = new int[n];
+            if (n == 1) {
+                System.out.println(1);
+            } else {
+                for (int h = 1; h <= n; h++) {
+                    hosu[h] = h; // 호수의 구성원은 그냥 h임
+                for (int j = 0; j <= k; j++) { // 층수
+                    if (j == 0) {   // 0층일때
+                        System.out.println(hosu[j]);
+                        }
+                    } else {    // 그렇지 않으면 0층 이상일 땐,
+                        for (int a = 1; a <= n; a++) {
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+        // 자신이 a층의 b호에 살기 위해서는 
+        // 자신의 아랫층에 1호부터 b호까지의 합만큼 사람을 데리고 와야함
+
+    }
+}
--- baek_2869.java
+++ baek_2869.java
@@ -1,21 +1,23 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
-public class baek_2869 {
-    public static void main(String[] args) throws IOException  {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int up = Integer.parseInt(br.readLine());
-        int down = Integer.parseInt(br.readLine());
-        int length = Integer.parseInt(br.readLine());
-        int day = (length - down) / (up - down);
-        
-        if ( ((length - down) % (up - down)) != 0) {
-            day++;
-        }
-        System.out.println(day);
  
-    }   
+public class baek_2869 {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+		int up = Integer.parseInt(st.nextToken());
+		int down = Integer.parseInt(st.nextToken());
+		int length = Integer.parseInt(st.nextToken());
+ 
+		int day = (length - down) / (up - down);
+		if ((length - down) % (up - down) != 0)
+			day++;
+ 
+		System.out.println(day);
+	}
 }
