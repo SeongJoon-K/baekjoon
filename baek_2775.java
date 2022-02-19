4ad104ee5a078ecec0cfc161331049e3b664db70
SeongJoon-K <jobcho6320@naver.com>
2022-02-19 17:16:12 +0900

    219_2775 부녀회장

--- baek_2775.java
+++ baek_2775.java
@@ -5,27 +5,23 @@ public class baek_2775 {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt(); // 테스트 횟수
         // 내가 2층 3호에 살려면, 101호, 102호, 103호의 사람수의 합만큼 데리고 와야함
-        
-        for (int i = 0; i < T; i++) {
+        int[][] APT = new int[15][15];
+        for (int a = 0; a < 15; a++) {
+            APT[0][a] = a;
+            APT[a][1] = 1;
+        }
+
+        for (int i = 1; i <= T; i++) {
             int k = sc.nextInt(); // 층 수
             int n = sc.nextInt(); // 호 수
-            int[] height = new int[k];
-            int[] hosu = new int[n];
-            if (n == 1) {
-                System.out.println(1);
-            } else {
-                for (int h = 1; h <= n; h++) {
-                    hosu[h] = h; // 호수의 구성원은 그냥 h임
-                for (int j = 0; j <= k; j++) { // 층수
-                    if (k == 0) {   // 0층일때
-                        System.out.println(hosu[j]);
-                    } else {
-                        for (int a = 1; a <=n; a++) {
-                            
-                        }
-                    }
+            for (int j = 1; j < 15; j++) { // 1층 14층
+
+
+                for (int h = 2; h < 15; h++) { // 2호부터 14호
+                    APT[j][h] = APT[j][h-1] + APT[j-1][h];
                 }
             }
+            System.out.println(APT[k][n]);
         }
 
         // 자신이 a층의 b호에 살기 위해서는 
