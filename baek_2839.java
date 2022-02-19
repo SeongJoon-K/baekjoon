6c468023e291fae5a0d2b6e4e31ed2acee9f0855
SeongJoon-K <jobcho6320@naver.com>
2022-02-20 00:53:13 +0900

    220_2839

--- baek_2839.java
+++ baek_2839.java
@@ -6,12 +6,27 @@ public class baek_2839 {
 
         // 설탕공장 봉지는 3, 5kg
         int suga = sc.nextInt();
-        
-        if (suga % 5 == 0) {
+        if (suga % 10 == 3) {
+            System.out.println(suga/5 + 1);
+        } else if (suga % 10 == 4 && suga > 10) {
+            System.out.println((suga-9)/5+3);
+        } else if (suga % 10 == 5) {
             System.out.println(suga/5);
-        } else if (suga % 5 == 3) {
-            System.out.println(suga/5 +1);
-        } else if (suga % 5 != 3 || suga % 5 != 0) {
+        } else if (suga % 10 == 6) {
+            System.out.println((suga-6)/5 +2);
+        } else if (suga % 10 == 7 && suga >10) {
+            System.out.println((suga-12)/5+4);
+        } else if (suga % 10 == 8) {
+            System.out.println((suga-3)/5+1);
+        } else if (suga % 10 == 9) {
+            System.out.println((suga-9)/5+3);
+        } else if (suga % 10 == 0) {
+            System.out.println(suga/5);
+        } else if (suga % 10 == 1) {
+            System.out.println((suga-6)/5+2);
+        } else if (suga % 10 == 2) {
+            System.out.println((suga-12)/5+4);
+        } else {
             System.out.println(-1);
         }
     }
