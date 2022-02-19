160a6e27c8b5ae1afc775c63b3fd8a0b47163493
SeongJoon-K <jobcho6320@naver.com>
2022-02-19 17:55:44 +0900

    219 2839

--- baek_2839.java
+++ baek_2839.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
- 
+
 public class baek_2839 {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 설탕공장 봉지는 3, 5kg
+        int suga = sc.nextInt();
         
-		int N = in.nextInt();
-		
-		
-		if (N == 4 || N == 7) {
-			System.out.println(-1);
-		}
-		else if (N % 5 == 0) {
-			System.out.println(N / 5);
-		}
-		else if (N % 5 == 1 || N % 5 == 3) {
-			System.out.println((N / 5) + 1);
-		}
-		else if (N % 5 == 2 || N % 5 == 4) {
-			System.out.println((N / 5) + 2);
-		}
-	}
+        if (suga % 5 == 0) {
+            System.out.println(suga/5);
+        } else if (suga % 5 == 3) {
+            System.out.println(suga/5 +1);
+        } else if (suga % 5 != 3 || suga % 5 != 0) {
+            System.out.println(-1);
+        }
+    }
+    
 }
- 
