08c390954e73386ab520a9f08afe6c43f46d6500
SeongJoon-K <jobcho6320@naver.com>
2022-02-26 17:31:14 +0900

    226_3009

--- baek_3009.java
+++ baek_3009.java
@@ -1,7 +1,34 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class baek_3009 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] x_arr = new int[3];
+        int[] y_arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            x_arr[i] = x;
+            y_arr[i] = y;
+        }
+
+        if(x_arr[0] == x_arr[1]) {
+            System.out.print(x_arr[2] + " ");
+        } else if (x_arr[0] == x_arr[2]) {
+            System.out.print(x_arr[1] + " ");
+        } else if (x_arr[1] == x_arr[2]) {
+            System.out.print(x_arr[0] + " ");
+        }
+
+        if(y_arr[0] == y_arr[1]) {
+            System.out.print(y_arr[2]);
+        } else if (y_arr[0] == y_arr[2]) {
+            System.out.print(y_arr[1]);
+        } else if (y_arr[1] == y_arr[2]) {
+            System.out.print(y_arr[0]);
+        }
+        
     }
 }
