55e1cecdc5bb36926e37eb6d286524718ad40abd
SEONGJOON KIM <jobcho6320@naver.com>
2022-03-20 02:42:38 +0900

    320_farm_test

---
+++ farm_4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class farm_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String input;
+        input = sc.nextLine();
+        if (input.equals("+")) {
+            System.out.println(a + b);
+        } else if (input.equals("-")) {
+            System.out.println(a - b);
+        } else if (input.equals("*")) {
+            System.out.println(a * b);
+        } else if (input.equals("/")) {
+            System.out.println(a / b);
+        }
+    }
+}
