6c1d04c07c49d03f6af51ef57b0e2c20036dd96c
SEONGJOON KIM <jobcho6320@naver.com>
2022-03-29 23:39:56 +0900

    329_2750

--- baek_2447.java
+++ baek_2447.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 
 public class baek_2447 {
+
+    public static void jae() {
+        int[][] star = new int[3][3];
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y ++) {
+                if[]
+            }
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         // 27의 패턴은 가운데에 9*9 정사각형을
         // 9의 패턴으로 둘러싼 형태임
-        // 9의 패턴은 
+        // 9의 패턴은
         // 3*3정사각형의 크기를 3패턴으로 둘러싼거
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        for (int i = 0; i < N; i++){
+
+        for (int i = 0; i < N; i++) {
 
         }
     }
