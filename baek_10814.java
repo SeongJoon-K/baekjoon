6576cb3a6e69c75bcb14f41ba549be8f48c084f8
SEONGJOON KIM <jobcho6320@naver.com>
2022-04-04 00:48:32 +0900

    404_10814

---
+++ baek_10814.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Map.Entry;
+
+public class baek_10814 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 백준 회원수 입력받기
+        String[][] arr = new String[N][2];
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.next(); // 나이
+            arr[i][1] = sc.next();
+
+        }
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+
+    }
+}
+/*
+ * 
+ * 어떻
+ * 1번 해쉬맵 int, string 형 입력받고
+ * 그 다음 해쉬맵 정렬한다.
+ */
